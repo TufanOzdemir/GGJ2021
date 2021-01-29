0d396865b85a1d79b6f48157c8de240ef03ea103
Androuinn <tugay.mandal@gmail.com>
2021-01-29 22:20:05 +0300

    Player mover script

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+
+    Rigidbody rb;
+    [SerializeField] float speed = 10f;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        rb.MovePosition(transform.position + (new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * Time.deltaTime * speed));
+
+
+        
+
+
+    }
+}
