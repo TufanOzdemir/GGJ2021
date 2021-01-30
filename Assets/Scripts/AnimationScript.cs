e0aeec59591f70f03cc6ae7cd49437ecc1ea4223
Androuinn <tugay.mandal@gmail.com>
2021-01-30 14:28:52 +0300

    Saldırı animasyonu yapıldı

---
+++ Assets/Scripts/AnimationScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationScript : MonoBehaviour
+{
+
+    private Animator _animator;
+    bool secondAttack = false;
+
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+       
+
+        var attackState = _animator.GetCurrentAnimatorStateInfo(0).IsName("Attack");
+
+        if (attackState != true && Input.GetAxisRaw("Fire1") != 0)
+        {
+            _animator.SetTrigger("Attack");
+        }
+
+        if (attackState == true && Input.GetAxisRaw("Fire1") != 0 && secondAttack == false)
+        {
+            secondAttack = true;
+        }
+
+        //if (attackState && _animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1 && secondAttack == true)
+        //{
+        //    _animator.Play("New State");
+        //    secondAttack = false;
+        //}
+
+        _animator.SetFloat("Blend", Input.GetAxis("Vertical") * GetComponent<Mover>()._stats.MoveSpeed);
+    }
+}
