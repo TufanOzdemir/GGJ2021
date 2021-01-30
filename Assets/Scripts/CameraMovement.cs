43e0d1943cf6793eff8b050f407b96e18d5c6517
Androuinn <tugay.mandal@gmail.com>
2021-01-30 12:22:53 +0300

    Mover script güncelleme

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    private float speedH = 2f;
+    private float speedV = 2f;
+
+    private float yaw = 0f;
+    private float pitch = 0f;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = transform.parent.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch += speedV * Input.GetAxis("Mouse Y");
+
+        Quaternion deltaRotation = Quaternion.Euler(new Vector3(0, yaw, 0) * Time.deltaTime);
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0f);
+        
+        //rb.MoveRotation(transform.rotation * deltaRotation);
+    }
+}
--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -7,6 +7,7 @@ public class Mover : MonoBehaviour
     private PlayerStats _stats;
     private Vector3 _moveVector;
     private Animator _animator;
+    private CameraMovement asd;
 
     private void Awake()
     {
@@ -14,6 +15,7 @@ public class Mover : MonoBehaviour
         _moveVector = new Vector3();
         _stats = GetComponent<PlayerStats>();
         _animator = GetComponent<Animator>();
+        asd = GetComponentInChildren<CameraMovement>();
     }
 
     void Start()
@@ -23,8 +25,15 @@ public class Mover : MonoBehaviour
 
     void Update()
     {
-        _moveVector.Set(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        _rigidbody.MovePosition(transform.position + _moveVector * Time.deltaTime * _stats.MoveSpeed);
+
+        if (Input.GetAxisRaw("Vertical")>0)
+        {
+            _rigidbody.MovePosition( _rigidbody.transform.position + asd.transform.forward * _stats.MoveSpeed * Time.deltaTime);
+        }
+
+        //_moveVector.Set(Input.GetAxis("Horizontal"), 0,  Input.GetAxis("Vertical"));
+        //_rigidbody.MovePosition(rigidbody.transform.forward  * Time.deltaTime * _stats.MoveSpeed);
+
         _animator.SetFloat("Blend", Input.GetAxis("Vertical") * _stats.MoveSpeed);
     }
 }
