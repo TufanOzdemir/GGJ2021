8ceb03a33e5f72ea85f5b1f49cbfd50e98c4294b
TufanOzdemir <amonisal6@gmail.com>
2021-01-30 09:33:46 +0300

    Vk

---
+++ Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    Rigidbody _rigidbody;
+    PlayerStats _playerStats;
+    public bool _isAlarm;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _playerStats = GetComponent<PlayerStats>();
+        _isAlarm = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_isAlarm)
+        {
+            //Kullanıcı takip etme kodları buraya konmalı
+        }
+    }
+}
